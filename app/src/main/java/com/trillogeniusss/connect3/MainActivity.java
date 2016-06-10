bba20cf915328e15ea214abb0f27e57f1611a18a
Ayobami E Ige <thorbaba01@gmail.com>
2016-06-09 21:23:04 -0500

    Updated the winning and play again logic

--- app/src/main/java/com/trillogeniusss/connect3/MainActivity.java
+++ app/src/main/java/com/trillogeniusss/connect3/MainActivity.java
@@ -4,48 +4,53 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
-    int turn = 0;
-    int [] board = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
-    int [][] winningPos = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int turn = 0;// 0 is for player 1 and 1 is for player 2
+    int [] board = {-1,-1,-1,-1,-1,-1,-1,-1,-1};//keeps track of the used position on the grid, set to a default of -1
+    int [][] winningPos = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};// array of possible winning positions on the grid
+    boolean isActive = true;// checks if the bors is still active
+
     public void taptap(View view){
 	ImageView counter1 = (ImageView)view;
 	int pos = Integer.parseInt(counter1.getTag().toString());
-	if(board[pos]== -1){
+
+	if(board[pos]== -1 && isActive){
+
 	    board[pos] = turn;
 	    counter1.setTranslationY(-1000f);
+
 	    if(turn == 0){
 		counter1.setImageResource(R.drawable.single);
 	    }else{
 		counter1.setImageResource(R.drawable.inner);
 	    }
+
 	    counter1.animate().translationYBy(1000f).setDuration(30);
-	    boolean isWinner = checkWinner(turn);
+
+	    checkWinner(turn);
 
 	    if(turn == 0){
 		turn =1;
 	    }else{
 		turn =0;
 	    }
-
-	}else{
-	    Toast.makeText(getApplicationContext(), "Position Taken : Try a different Location.", Toast.LENGTH_LONG ).show();
 	}
     }
 
     public boolean checkWinner(int turn){
 	int count = 0;
 	boolean winner = false;
+
 	for (int i = 0; i < winningPos.length; i++){
 	    for(int j = 0; j< winningPos[i].length; j++) {
 		int val = winningPos[i][j];
@@ -58,17 +63,39 @@ public class MainActivity extends Activity {
 		    break;
 		}
 	    }
+
 	    if(count == 3){
 		LinearLayout winnerMsg = (LinearLayout)findViewById(R.id.playagainlayout);
 		TextView winnerText = (TextView)findViewById(R.id.winnerstext);
 		winnerText.setText("Player " + (turn+1) + " Wins!!!!");
 		winnerMsg.setVisibility(View.VISIBLE);
-		//Toast.makeText(getApplicationContext(), "Player " + (turn+1) + " wins", Toast.LENGTH_LONG ).show();
 		winner = true;
+		isActive = false;
 		break;
+	    }
+	    else{
+		count = 0;
 	    }
 	}
 	return winner;
+    }
+
+    public void playAgain(View view){
+
+	LinearLayout winnerMsg = (LinearLayout)findViewById(R.id.playagainlayout);
+	winnerMsg.setVisibility(View.INVISIBLE);
+	turn = 0;
+	isActive = true;
+
+	for(int j =0 ; j<board.length; j++) {
+	    board[j] = -1;
+	}
+
+	GridLayout grid = (GridLayout)findViewById(R.id.grid );
+
+	for (int i =0; i < grid.getChildCount();i++){
+	    ((ImageView) grid.getChildAt(i)).setImageResource(0);
+	}
     }
 
     @Override
